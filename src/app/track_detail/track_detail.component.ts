769f1d48ba50cdb66a3712e9ec5fbdd56b78dd3d
Ahmet Kizilay <ahmet.kizilay@gmail.com>
2024-07-01 21:16:33 -0400

    remove audio service dependency for track detail component

--- src/app/track_detail/track_detail.component.ts
+++ src/app/track_detail/track_detail.component.ts
@@ -1,7 +1,6 @@
 import { Component, Input, OnChanges, OnInit, SimpleChanges, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { AudioService, TrackParams } from '../audio.service';
-import { Track } from '../scene.service';
+import { Track, SampleTrackParams } from '../scene.service';
 
 @Component({
   selector: 'app-track-detail',
@@ -22,29 +21,28 @@ import { Track } from '../scene.service';
     <label class="param-name">Gain:</label>
     <input data-param="gain" type="range" min="0" max="100"
       (input)="onGainChange($event)"
-      value="{{trackParams.gain * 100}}"/>
+      value="{{trackParams.gain! * 100}}"/>
   </div>
   <div class="param-block">
     <label class="param-name">Pan:</label>
     <input data-param="pan" type="range" min="0" max="100"
       (input)="onPanChange($event)"
-      value="{{(trackParams.pan * 50) + 50}}"/>
+      value="{{(trackParams.pan! * 50) + 50}}"/>
   </div>
   <div class="param-block">
     <label class="param-name">Delay Send:</label>
     <input data-param="delaySend"  type="range" min="0" max="100"
       (input)="onDelaySendChange($event)"
-      value="{{trackParams.delaySend * 100}}"/>
+      value="{{trackParams.delaySend! * 100}}"/>
   </div>
 </div>
   `,
   styleUrls: ['./track_detail.component.css']
 })
 export class TrackDetailComponent implements OnInit, OnChanges {
-  private audio: AudioService = inject(AudioService);
 
   @Input({ required: true }) track!: Track;
-  trackParams = {} as TrackParams;
+  trackParams = {} as SampleTrackParams;
 
   ngOnChanges(changes: SimpleChanges): void {
     if (changes['track']) {
@@ -59,19 +57,16 @@ export class TrackDetailComponent implements OnInit, OnChanges {
   onGainChange(e: Event) {
     const el = e.target as HTMLInputElement;
     this.trackParams.gain = el.valueAsNumber / 100;
-    this.audio.setTrackParams(this.track.name, this.trackParams);
   }
 
   onDelaySendChange(e: Event) {
     const el = e.target as HTMLInputElement;
     this.trackParams.delaySend = el.valueAsNumber / 100;
-    this.audio.setTrackParams(this.track.name, this.trackParams);
   }
 
   onPanChange(e: Event) {
     const el = e.target as HTMLInputElement;
     this.trackParams.pan = (el.valueAsNumber - 50.) / 50.;
-    this.audio.setTrackParams(this.track.name, this.trackParams);
   }
 
   handleDrop(ev: Event) {
@@ -81,7 +76,6 @@ export class TrackDetailComponent implements OnInit, OnChanges {
     const data = e.dataTransfer?.getData("sample");
     if (data) {
       this.trackParams.sampleId = data;
-      this.audio.setTrackParams(this.track.name, this.trackParams);
     }
 
     return false;
@@ -95,20 +89,11 @@ export class TrackDetailComponent implements OnInit, OnChanges {
     if (!this.track) {
       return;
     }
-    let params = this.audio.getTrackParams(this.track.name);
-    if (params) {
-      this.trackParams = params;
 
-      // Ensure that the values are valid.
-      if (isNaN(this.trackParams.gain)) {
-        this.trackParams.gain = 1.0;
-      }
-      if (isNaN(this.trackParams.pan)) {
-        this.trackParams.pan = 0.0;
-      }
-      if (isNaN(this.trackParams.delaySend)) {
-        this.trackParams.delaySend = 0.0;
-      }
-    }
+    this.trackParams = this.track.params;
+    // Ensure that the values are valid.
+    this.trackParams.gain = Number.isNaN(this.trackParams.gain) ? 1.0 : this.trackParams.gain;
+    this.trackParams.pan = Number.isNaN(this.trackParams.pan) ? 0.0 : this.trackParams.pan;
+    this.trackParams.delaySend = Number.isNaN(this.trackParams.delaySend) ? 0.0 : this.trackParams.delaySend;
   }
 }
