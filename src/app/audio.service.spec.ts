afe366e31fc7d3abbe97a469e0736fa2b48cc982
Ahmet Kizilay <ahmet.kizilay@gmail.com>
2024-07-07 16:31:39 -0400

    create initial test for audio ervice

--- src/app/audio.service.spec.ts
+++ src/app/audio.service.spec.ts
@@ -1,16 +1,25 @@
 import { TestBed } from '@angular/core/testing';
 
+import { MockAudioContext } from './mocks/audio_context';
 import { AudioService } from './audio.service';
+import { SampleLibraryService } from './sample_library/sample_library.service';
 
-xdescribe('AudioService', () => {
+describe('AudioService', () => {
   let service: AudioService;
+  let mockAudioContext: MockAudioContext;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    mockAudioContext = new MockAudioContext();
+    TestBed.configureTestingModule({
+      providers: [
+        { provide: AudioContext, useValue: mockAudioContext},
+        { provide: SampleLibraryService, useValue: {}},
+      ],
+    });
     service = TestBed.inject(AudioService);
   });
 
-  it('should be created', () => {
+  it('creates the service', () => {
     expect(service).toBeTruthy();
   });
 });
--- src/app/home/home.component.spec.ts
+++ src/app/home/home.component.spec.ts
@@ -4,32 +4,7 @@ import { HomeComponent } from './home.component';
 import { SampleLibraryService } from '../sample_library/sample_library.service';
 import { Subject } from 'rxjs';
 import { By } from '@angular/platform-browser';
-
-class MockAudioContext {
-  state: string = 'suspended';
-  registeredEvents: Map<string, Function[]> = new Map();
-
-  addEventListener(eventName:string, fn: Function) {
-    let currentRegisteredEvents = this.registeredEvents.get(eventName) || [];
-    this.registeredEvents.set(eventName, [...currentRegisteredEvents, fn]);
-  }
-  removeEventListener(eventName: string, fn: Function) {
-    let currentRegisteredEvents = this.registeredEvents.get(eventName) || [];
-    this.registeredEvents.set(eventName, currentRegisteredEvents.filter((f) => f !== fn));
-  }
-
-  resume() {
-    this.state = 'running';
-    let currentRegisteredEvents = this.registeredEvents.get('statechange') || [];
-    currentRegisteredEvents.forEach((fn) => fn());
-  }
-
-  mockSuspend() {
-    this.state = 'suspended';
-    let currentRegisteredEvents = this.registeredEvents.get('statechange') || [];
-    currentRegisteredEvents.forEach((fn) => fn());
-  }
-}
+import { MockAudioContext } from '../mocks/audio_context';
 
 describe('HomeComponent', () => {
   let component: HomeComponent;
