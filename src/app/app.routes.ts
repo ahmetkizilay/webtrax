1bd63eb5f36dfe3249ca3a528b0a86ada5b68d9c
Ahmet Kizilay <ahmet.kizilay@gmail.com>
2024-07-07 17:01:45 -0400

    move auth related code to login directory

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,7 +2,7 @@
 import { Routes } from '@angular/router'; // CLI imports router
 import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
-import { allWelcome, requireLogin } from './auth-guard.guard';
+import { allWelcome, requireLogin } from './login/auth-guard.guard';
 import { SamplesComponent } from './samples/samples.component';
 
 export const routes: Routes = [
--- src/app/login/login.component.spec.ts
+++ src/app/login/login.component.spec.ts
@@ -1,7 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { LoginComponent } from './login.component';
-import { AuthService } from '../auth.service';
+import { AuthService } from './auth.service';
 import { Subject } from 'rxjs';
 import { By } from '@angular/platform-browser';
 
