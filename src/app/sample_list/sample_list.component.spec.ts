e7bf6c04534f515bd839fa87d6aeacb1e4abee65
Ahmet Kizilay <ahmet.kizilay@gmail.com>
2024-07-07 16:49:35 -0400

    add tests for the sample list component

--- src/app/sample_list/sample_list.component.spec.ts
+++ src/app/sample_list/sample_list.component.spec.ts
@@ -1,13 +1,31 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { SampleListComponent } from './sample_list.component';
+import { Sample, SampleLibraryService } from '../sample_library/sample_library.service';
+import { AudioService } from '../audio.service';
+import { Subject } from 'rxjs';
+import { By } from '@angular/platform-browser';
 
-xdescribe('SampleListComponent', () => {
+describe('SampleListComponent', () => {
   let component: SampleListComponent;
   let fixture: ComponentFixture<SampleListComponent>;
 
+  let mockPlaySampleFn: jasmine.Spy;
+  let mockSamples$: Subject<Sample[]>;
+
   beforeEach(() => {
+    mockSamples$ = new Subject<Sample[]>();
+    mockPlaySampleFn = jasmine.createSpy();
+
     TestBed.configureTestingModule({
+      providers: [
+        { provide: SampleLibraryService, useValue: {
+          samples$: mockSamples$,
+        }},
+        { provide: AudioService, useValue: {
+          playSample: mockPlaySampleFn,
+        }},
+      ],
       imports: [SampleListComponent]
     });
     fixture = TestBed.createComponent(SampleListComponent);
@@ -15,7 +33,29 @@ xdescribe('SampleListComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('creates component', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('lists samples', () => {
+    mockSamples$.next([
+      {name: 'public/test.wav', path: 'public/test.wav'},
+      {name: 'public/test2.wav', path: 'public/test2.wav'}
+    ]);
+    fixture.detectChanges();
+    const sampleList = Array.from(fixture.debugElement.queryAll(By.css('.sample')));
+    expect(sampleList.length).toBe(2);
+  });
+
+  it('plays sample', () => {
+    mockSamples$.next([
+      {name: 'public/test.wav', path: 'public/test.wav'},
+    ]);
+    fixture.detectChanges();
+
+    const playButton = fixture.debugElement.query(By.css('.sample button'));
+    playButton.triggerEventHandler('click', null);
+
+    expect(mockPlaySampleFn).toHaveBeenCalledWith('test');
   });
 });
