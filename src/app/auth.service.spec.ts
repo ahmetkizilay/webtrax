dcc5e5ef8e2f4b2466cf8df6e6f40f3c31c8e4be
Ahmet Kizilay <ahmet.kizilay@gmail.com>
2024-07-07 15:47:45 -0400

    basic test for auth service

--- src/app/auth.service.spec.ts
+++ src/app/auth.service.spec.ts
@@ -1,12 +1,38 @@
 import { TestBed } from '@angular/core/testing';
 
 import { AuthService } from './auth.service';
+import { getApp, initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { connectAuthEmulator } from '@firebase/auth';
 
-xdescribe('AuthService', () => {
+describe('AuthService', () => {
+  const projectId = 'webtrax-1fc7d';
   let service: AuthService;
+  let appName: string;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    appName = `authService-${Date.now()}`;
+    TestBed.configureTestingModule({
+      providers: [
+        provideFirebaseApp(() =>
+          initializeApp(
+            {
+              projectId,
+              apiKey: 'AIzaSyD',
+            },
+            appName
+          )
+        ),
+        provideAuth(() => {
+          let auth = getAuth(getApp(appName));
+          connectAuthEmulator(auth, 'http://localhost:9099', {
+            disableWarnings: true,
+          });
+          return auth;
+        }),
+      ],
+    });
+
     service = TestBed.inject(AuthService);
   });
 
